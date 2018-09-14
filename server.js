5c2cfd3fa12acd4c0be19ee65ce6d166dbbf8230
Roberth Borges <roberth.borges@externos-cl.cencosud.com>
2018-09-14 00:00:57 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const hbs = require('hbs')
+require('./hbs/helpers')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'roBERth borges'
+    });
+})
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+})
